e3c46bbe1d612e9d75a0095c2dc3b7597acc1dbc
Paul Charbonneau <paulcharbo@gmail.com>
2023-07-17 18:00:33 +0200

    Encoder

---
+++ encoder.go
@@ -0,0 +1,76 @@
+package bencode
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
+type encoder struct {
+	output bytes.Buffer
+}
+
+func (e *encoder) init() *encoder {
+	return &encoder{bytes.Buffer{}}
+}
+
+func (e *encoder) encodeString(value string) {
+	e.output.WriteString(fmt.Sprintf("%d", len(value)))
+	e.output.WriteByte(':')
+	e.output.WriteString(value)
+}
+
+func (e *encoder) encodeUint(value uint64) {
+	e.output.WriteByte('i')
+	e.output.WriteString(fmt.Sprintf("%d", value))
+	e.output.WriteByte('e')
+}
+func (e *encoder) encodeList(rv reflect.Value) {
+	e.output.WriteByte('l')
+	indirect := reflect.Indirect(rv)
+	for i := 0; i < indirect.Len(); i++ {
+		switch indirect.Index(i).Kind() {
+		case reflect.Uint64:
+		case reflect.String:
+			e.encodeString(indirect.Index(i).Interface().(string))
+		case reflect.Struct:
+		case reflect.Slice:
+			e.encodeList(indirect.Index(i).Addr())
+		}
+	}
+	e.output.WriteByte('e')
+}
+
+func (e *encoder) encodeDict(rv reflect.Value) error {
+	e.output.WriteByte('d')
+	indirect := reflect.Indirect(rv)
+	for i := 0; i < indirect.NumField(); i++ {
+		switch indirect.Type().Field(i).Type.Kind() {
+		case reflect.Uint64:
+			e.encodeString(indirect.Type().Field(i).Tag.Get("bencode"))
+			e.encodeUint(indirect.Field(i).Interface().(uint64))
+		case reflect.String:
+			e.encodeString(indirect.Type().Field(i).Tag.Get("bencode"))
+			e.encodeString(indirect.Field(i).Interface().(string))
+		case reflect.Struct:
+			e.encodeString(indirect.Type().Field(i).Tag.Get("bencode"))
+			e.encodeDict(indirect.Field(i).Addr())
+		case reflect.Slice:
+			e.encodeString(indirect.Type().Field(i).Tag.Get("bencode"))
+			e.encodeList(indirect.Field(i).Addr())
+		}
+	}
+	e.output.WriteByte('e')
+	return nil
+}
+
+func (e *encoder) marshal(v any) ([]byte, error) {
+	e.encodeDict(reflect.ValueOf(v))
+	return e.output.Bytes(), nil
+}
+
+func encode(v any) ([]byte, error) {
+	var e encoder
+	e.init()
+	return e.marshal(v)
+}
---
+++ encoder_test.go
@@ -0,0 +1,73 @@
+package bencode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEcodeInt(t *testing.T) {
+	type test struct {
+		Test uint64 `bencode:"test"`
+	}
+	have, err := encode(&test{1234567890})
+	want := []byte("d4:testi1234567890ee")
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(have, want) {
+		t.Errorf("Struct not properly encoded: wanted %s but have %s", want, have)
+	}
+}
+
+func TestEncodeString(t *testing.T) {
+	type test struct {
+		Test string `bencode:"test"`
+	}
+	have, err := encode(&test{"test"})
+	want := []byte("d4:test4:teste")
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(have, want) {
+		t.Errorf("Struct not properly encoded: wanted %s but have %s", want, have)
+	}
+}
+
+func TestEncodeStringInt(t *testing.T) {
+	type test struct {
+		TestString string `bencode:"teststring"`
+		TestInt uint64 `bencode:"testint"`
+	}
+	have, err := encode(&test{"test", 1234567890})
+	want := []byte("d10:teststring4:test7:testinti1234567890ee")
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(have, want) {
+		t.Errorf("Struct not properly encoded: wanted %s but have %s", want, have)
+	}
+}
+
+func TestEncodeMockTorrentFile(t *testing.T) {
+	type info struct {
+		Length uint64 `bencode:"length"`
+		Name string `bencode:"name"`
+		PieceLength uint64 `bencode:"piece length"`
+	}
+	type test struct {
+		Announce string `bencode:"announce"`
+		AnnounceList [][]string `bencode:"announce-list"`
+		Comment string `bencode:"comment"`
+		CreatedBy string `bencode:"created by"`
+		CreationDate uint64 `bencode:"creation date"`
+		Info info `bencode:"info"`
+	}
+	have, err := encode(&test{"https://torrent.ubuntu.com/announce", [][]string{{"https://torrent.ubuntu.com/announce"}, {"https://ipv6.torrent.ubuntu.com/announce"}}, "Ubuntu CD releases.ubuntu.com", "mktorrent 1.1", 1681992794, info{4932407296, "ubuntu-23.04-desktop-amd64.iso", 262144}})
+	want := []byte("d8:announce35:https://torrent.ubuntu.com/announce13:announce-listll35:https://torrent.ubuntu.com/announceel40:https://ipv6.torrent.ubuntu.com/announceee7:comment29:Ubuntu CD releases.ubuntu.com10:created by13:mktorrent 1.113:creation datei1681992794e4:infod6:lengthi4932407296e4:name30:ubuntu-23.04-desktop-amd64.iso12:piece lengthi262144eee")
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(have, want) {
+		t.Errorf("Struct not properly encoded: wanted %s but have %s", want, have)
+	}
+}
